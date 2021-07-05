98c7a2e8541b18a3105de0cf9b366f267e8547cd
Aaron <pengjieran@gmail.com>
2021-07-05 22:51:17 +0800

    修改错误

--- src/main/java/com/examplecn/netty/server/DiscardServer.java
+++ src/main/java/com/examplecn/netty/server/DiscardServer.java
@@ -3,12 +3,12 @@ package com.examplecn.netty.server;
 import com.examplecn.netty.server.handler.DiscardServerHandler;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 public class DiscardServer {
@@ -26,9 +26,10 @@ public class DiscardServer {
 		EventLoopGroup workGroup = new NioEventLoopGroup();
 		
 		ServerBootstrap serverBootstrap = new ServerBootstrap();
-		serverBootstrap.group(mainGroup, workGroup).channel(NioServerSocketChannel.class).childHandler(new ChannelInitializer<Channel>() {
+		serverBootstrap.group(mainGroup, workGroup).channel(NioServerSocketChannel.class).childHandler(new ChannelInitializer<SocketChannel>() {
 			
-			protected void initChannel(Channel channel) throws Exception {
+			@Override
+			protected void initChannel(SocketChannel channel) throws Exception {
 				
 				channel.pipeline().addLast(new DiscardServerHandler());
 			};
